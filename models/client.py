a352edb9cdd0408eafceb033ab8de12f115c12a8
Nahuel <matiasghilardisalinas@gmail.com>
2025-04-24 23:41:04 -0300

    client.py modified

--- models/client.py
+++ models/client.py
@@ -1,15 +1,17 @@
 from models.db import db
 
+# Clase de clientes
 class Client(db.Model):
     __tablename__ = 'clients'
 
+    # Atributos de los clientes
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
     telefono = db.Column(db.String(20))
     email = db.Column(db.String(120), unique=True)
 
-    # Relación con vehículos (uno a muchos)
+    # La relacion de los vehiculos es 1:N
     vehiculos = db.relationship('Vehicle', backref='cliente', lazy=True)
 
     def __repr__(self):
